f03aff6b2f5bcb48363c90e13bc61543dad2b1b9
Chris <rolletchris1@gmail.com>
2024-10-11 20:24:45 -0700

    Enhance documentation in sleep.c to clarify functionality, usage, and parameters

--- user/sleep.c
+++ user/sleep.c
@@ -1,10 +1,24 @@
-/*************************************************************************
-* sleep.c - sleep for a specified number of seconds
-*
-* Author: Chris Dedman
-* Date: 10/10/2024
-*
-**************************************************************************/
+/**************************************************************************
+ *
+ * @file sleep.c
+ * @brief A simple program to sleep for a specified number of seconds.
+ *
+ * This program takes a single command-line argument representing
+ * the number of seconds to sleep. It converts the seconds to
+ * ticks (assuming 10 ticks per second) and then calls the sleep function.
+ *
+ * Usage:
+ *   sleep <seconds>
+ *
+ * @param argc The number of command-line arguments.
+ * @param argv The array of command-line arguments.
+ *
+ * @return Returns 0 on successful completion, or exits with an error code if the input is invalid.
+ *
+ * @author Chris Dedman
+ * @date 10/10/2024
+ *
+ *************************************************************************/
 
 #include "kernel/types.h"
 #include "kernel/stat.h"
@@ -14,7 +28,7 @@ int main(int argc, char *argv[])
 {
   if (2 != argc)
   {
-    fprintf(2, "Usage: sleep <ticks>\n");
+    fprintf(2, "Usage: sleep <seconds>\n");
     exit(1);
   }
 
