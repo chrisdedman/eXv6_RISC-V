dbc31d4d739966d0d3011cb5e5794129bd27eeec
Chris <rolletchris1@gmail.com>
2024-10-11 20:23:54 -0700

    Enhance documentation in halt.c to clarify implementation and usage of the halt command

--- user/halt.c
+++ user/halt.c
@@ -1,8 +1,20 @@
-/*
- * Halt the system
- * Author: Chris Dedman
- * Date: 10/10/2024
- */
+/****************************************************************************
+ * 
+ * @file halt.c
+ * @brief This file contains the implementation of the halt command for the eXv6 RISC-V operating system.
+ *
+ * The halt command is intended to stop the operating system. 
+ * Currently, it is not implemented and simply prints a message
+ * instructing the user to manually shut down QEMU using Ctrl-a x.
+ *
+ * @note To-Do: Implement the halt system call.
+ *
+ * @return int Returns 0 upon completion.
+ *
+ * @author Chris Dedman
+ * @date 10/10/2024
+ *
+ ***************************************************************************/
 
 #include "kernel/types.h"
 #include "kernel/stat.h"
